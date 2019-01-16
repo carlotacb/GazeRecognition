d7cb4cac4cd2e6774de4b8d4d69304fc24f638c4
Carlota Catot Bragós <carlota.catot@est.fib.upc.edu>
2019-01-16 13:52:45 +0100

    last changes

--- src/gazeTesting.m
+++ src/gazeTesting.m
@@ -1,4 +1,4 @@
-function [] = gazeTesting()
+function [resultatNoMira, resultatMira, encert, prediction] = gazeTesting()
     Data = load('data\TestData.mat');
     classifier = load('data\gazeClassifier.mat');
     Data2 = load('data\GazeLabelsData.mat');
@@ -15,8 +15,10 @@ function [] = gazeTesting()
     prediction = predict(classifier.gazeClassifier, Features);
     
     cmatrix = confusionmat(Labels, prediction);
-    resultatNoMira = 100*cmatrix(1,1) / (cmatrix(1,1) + cmatrix(1,2))
-    resultatMira = 100*cmatrix(2,2) / (cmatrix(2,1) + cmatrix(2,2))
+    resultatNoMira = 100*cmatrix(1,1) / (cmatrix(1,1) + cmatrix(1,2));
+    resultatMira = 100*cmatrix(2,2) / (cmatrix(2,1) + cmatrix(2,2));
+    
+    encert = ((resultatMira*245)+(resultatNoMira*60))/length(Data.testingEyes);
     
     cchart = confusionchart(Labels, prediction);
 end
